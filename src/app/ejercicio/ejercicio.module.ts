1b9e48b501e12f8f1720ddd573d478a3335391cd
estudiante <estudiante@ISIS2603-423.sis.virtual.uniandes.edu.co>
2019-03-25 23:14:19 -0500

    correccion menor

--- src/app/ejercicio/ejercicio.module.ts
+++ src/app/ejercicio/ejercicio.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EjercicioComponent } from './ejercicio.component';
-import { EjercicioGETComponent } from './ejercicio-get/ejercicio-get.component';
-import { EjercicioListComponent } from './ejercicio-list/ejercicio-list.component';
 import { EjercicioDetailComponent } from './ejercicio-detail/ejercicio-detail.component';
+import { EjercicioListComponent } from './ejercicio-list/ejercicio-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [EjercicioComponent, EjercicioGETComponent, EjercicioListComponent, EjercicioDetailComponent]
+  declarations: [EjercicioDetailComponent, EjercicioListComponent]
 })
 export class EjercicioModule { }
