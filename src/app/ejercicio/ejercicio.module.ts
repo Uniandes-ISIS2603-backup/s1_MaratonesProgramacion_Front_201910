729b8967ae627cba8b7bf409ee0498713ef70f2d
estudiante <estudiante@ISIS2603-423.sis.virtual.uniandes.edu.co>
2019-03-27 06:53:52 -0500

    correccion menor

--- src/app/ejercicio/ejercicio.module.ts
+++ src/app/ejercicio/ejercicio.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EjercicioDetailComponent } from './ejercicio-detail/ejercicio-detail.component';
 import { EjercicioListComponent } from './ejercicio-list/ejercicio-list.component';
+import { EjercicioService } from './ejercicio.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: [EjercicioDetailComponent, EjercicioListComponent]
+  declarations: [EjercicioDetailComponent, EjercicioListComponent],
+  providers: [EjercicioService],
+  exports: [EjercicioDetailComponent, EjercicioListComponent]
 })
 export class EjercicioModule { }
