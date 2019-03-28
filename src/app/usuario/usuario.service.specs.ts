2b425ca099c25bb5ef80ac34c075129ab14f11e2
Camila Londono <camilalonart@Camilas-Air-3.uniandes.edu.co>
2019-03-28 13:29:37 -0500

    usuario SERVICE Y SERVICE.SPEC

--- src/app/usuario/usuario.service.specs.ts
+++ src/app/usuario/usuario.service.specs.ts
@@ -1,59 +1,13 @@
-import {ComponentFixture, TestBed, getTestBed} from '@angular/core/testing';
-import {APP_BASE_HREF} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
 
-import {Usuario} from './usuario';
-import {UsuarioService} from './usuario.service';
-import {AppModule} from '../app.module';
+import { TestBed } from '@angular/core/testing';
 
-describe('Service: UsuarioService', () => {
-    let injector: TestBed;
-    let service: UsuarioService;
-	const usuarios: Usuario[] = require('../../assets/usuarios.json');
-    
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [HttpClientModule, AppModule],
-            declarations: [],
-            providers: [{provide: APP_BASE_HREF, useValue: ''}, UsuarioService]
-        });
-        injector = getTestBed();
-        service = injector.get(UsuarioService);
-    });
-	
-	it('#getUsuarios should return value from observable',
-    (done: DoneFn) => {
-    service.getUsuarios().subscribe(value => {
-        expect(value.length).toBeGreaterThan(0);
-        done();
-        });
-    });
-    
-     it('#createUsuario should return value from observable',
-    (done: DoneFn) => {
-        let usuario:Usuario = {id:100,rol:"Participante",nombreUsuario:"camilalonart",nombre: "Camila",imagen:"https://farm2.staticflickr.com/1722/28018929057_d3aff402ac_b.jpg",clave:"aBc234$$bb",correo:"mc.londono@losAlpes.edu.co", puntaje:0};
-        service.createUsuario(usuario).subscribe(value => {
-        expect(value.nombreUsuario).toEqual(usuario.nombreUsuario);
-        done();
-        });
-    });
-	
-	it('#getUsuarioDetail should return value from observable',
-    (done: DoneFn) => {
-    service.getUsuarioDetail(usuarios[0].id).subscribe(value => {
-        expect(value.nombreUsuario).toEqual(usuarios[0].nombreUsuario);
-        done();
-        });
-    });
-	
-	
-	it('#updateUsuario should return the usuario updated',
-    (done: DoneFn) => {
-        let usuario:Usuario = {id:100,rol:"Participante",nombreUsuario:"camilalonart",nombre: "Camila",imagen:"https://farm2.staticflickr.com/1722/28018929057_d3aff402ac_b.jpg",clave:"aBc234$$bb",correo:"mc.londono@losAlpes.edu.co", puntaje:0};
-        service.updateUsuario(usuario).subscribe(value => {
-        expect(value.nombreUsuario).toEqual(usuario.nombreUsuario);
-        done();
-        });
-    });
-    
+import { UsuarioService } from './usuario.service';
+
+describe('UsuarioService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: UsuarioService = TestBed.get(UsuarioService);
+    expect(service).toBeTruthy();
+  });
 });
--- src/app/usuario/usuario.service.ts
+++ src/app/usuario/usuario.service.ts
@@ -1,60 +1,27 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import {Usuario} from './usuario';
-import {UsuarioDetail} from './usuario-detail';
 
 
-import {environment} from '../../environments/environment';
-const API_URL = environment.apiURL;
+const API_URL = "http://localhost:8080/s1_maratones-api";
 const usuarios = '/usuarios';
 
-
-/**
-* The service provider for everything related to usuarios
-*/
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class UsuarioService {
 
-    /**
+  /**
     * Constructor of the service
     * @param http The HttpClient - This is necessary in order to perform requests
     */
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-    /**
-    * Returns the Observable object containing the list of usuarios retrieved from the API
-    * @returns The list of usuarios in real time
-    */
-    getUsuarios(): Observable<Usuario[]> {
-        return this.http.get<Usuario[]>(API_URL + usuarios);
-    }
 
-    /**
-    * Creates a new usuario
-    * @param usuario The new usuario
-    * @returns The usuario with its new id if it was created, false if it wasn't
-    */
-    createUsuario(usuario): Observable<UsuarioDetail> {
-        return this.http.post<UsuarioDetail>(API_URL + usuarios, usuario);
-    }
+  getEjercicios() : Observable<Usuario[]>
+  {
+    return this.http.get<Usuario[]>(API_URL + usuarios);
+  }
 
-    /**
-    * Returns the Observable object with the details of an author retrieved from the API
-    * @returns The author details
-    */
-    getUsuarioDetail(usuarioId): Observable<UsuarioDetail> {
-        return this.http.get<UsuarioDetail>(API_URL + usuarios + '/' + usuarioId);
-    }
 
-   
-    /**
-        * Updates a new usuario
-        * @param usuario The updated usuario
-        * @returns The updated usuario
-        */
-    updateUsuario(usuario): Observable<UsuarioDetail> {
-        return this.http.put<UsuarioDetail>(API_URL + usuarios + '/' + usuario.id, usuario);
-    }
-}
