8e2632def4fd55f773b974be0b602952bcc69b24
JuanFelipePena <jf.pena10@uniandes.edu.co>
2019-04-09 16:42:55 -0500

    Update foro-detail.component.ts

--- src/app/foro/foro-detail/foro-detail.component.ts
+++ src/app/foro/foro-detail/foro-detail.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild, ViewContainerRef} from '@angular/core';
+import {ActivatedRoute, Router, NavigationEnd} from '@angular/router';
+import {ModalDialogService, SimpleModalComponent} from 'ngx-modal-dialog';
+import {ToastrService} from 'ngx-toastr';
+
+import {ForoService} from '../foro.service';
+import {Foro} from '../foro';
+import {ForoDetail} from '../foro-detail';
 
 @Component({
   selector: 'app-foro-detail',
   templateUrl: './foro-detail.component.html',
   styleUrls: ['./foro-detail.component.css']
 })
-export class ForoDetailComponent implements OnInit {
+export class ForoDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+        private foroService: ForoService,
+        private route: ActivatedRoute,
+        private modalDialogService: ModalDialogService,
+        private router: Router,
+        private viewRef: ViewContainerRef,
+        private toastrService: ToastrService) { }
 
   ngOnInit() {
   }
