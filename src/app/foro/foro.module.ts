944d5a030a3a5cb4bee561c255d6897c5965bf9f
JuanFelipePena <jf.pena10@uniandes.edu.co>
2019-04-09 16:32:11 -0500

    foro modulo completo

--- src/app/foro/foro.module.ts
+++ src/app/foro/foro.module.ts
@@ -1,16 +1,29 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from '../app-routing/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgxPermissionsModule} from 'ngx-permissions';
+
 import { ForoService } from './foro.service';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ForoListComponent } from './foro-list/foro-list.component';
 import { ForoDetailComponent } from './foro-detail/foro-detail.component';
+import { ForoCreateComponent } from './foro-create/foro-create.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        CommonModule,
+        FormsModule,
+        NgbModule,
+        ReactiveFormsModule,
+        NgxPermissionsModule
   ],
-  declarations: [ForoListComponent, ForoDetailComponent],
+  declarations: [ForoListComponent, ForoDetailComponent, ForoCreateComponent],
   providers: [ForoService],
   exports: [ForoDetailComponent, ForoListComponent]
 })
