7e4b078a00e8a96682627a87269eb3a43b89b393
JuanFelipePena <jf.pena10@uniandes.edu.co>
2019-04-10 14:41:31 -0500

    pruebas

--- src/app/foro/foro.module.ts
+++ src/app/foro/foro.module.ts
@@ -1,24 +1,23 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from '../app-routing/app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
+//import {BrowserModule} from '@angular/platform-browser';
+//import {AppRoutingModule} from '../app-routing/app-routing.module';
+//import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {NgxPermissionsModule} from 'ngx-permissions';
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//import {NgxPermissionsModule} from 'ngx-permissions';
 
 import { ForoService } from './foro.service';
 import { ForoListComponent } from './foro-list/foro-list.component';
 import { ForoDetailComponent } from './foro-detail/foro-detail.component';
 import { ForoCreateComponent } from './foro-create/foro-create.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-        BrowserModule,
-        HttpClientModule,
         CommonModule,
         FormsModule,
-        NgbModule
+        RouterModule
   ],
   declarations: [ForoListComponent, ForoDetailComponent, ForoCreateComponent],
   providers: [ForoService],
