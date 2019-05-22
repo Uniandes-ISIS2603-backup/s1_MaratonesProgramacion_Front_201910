4f00f1d7d81cad2e6dd73c5e5809453279cbc927
Camila Londono <camilalonart@Camilas-MacBook-Air-3.local>
2019-05-21 20:50:16 -0500

    cambios agregados

--- src/app/foro/foro-detail.ts
+++ src/app/foro/foro-detail.ts
@@ -1,10 +1,9 @@
 import { Comentario } from './../comentario/comentario';
 import { Foro } from "./foro";
-import {Usuario} from './../usuario/usuario';
 
 export class ForoDetail extends Foro{
 
+    
+    comentarios: Comentario[];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 
-    comentarios: Comentario[];
-    usuario: Usuario;
 }
