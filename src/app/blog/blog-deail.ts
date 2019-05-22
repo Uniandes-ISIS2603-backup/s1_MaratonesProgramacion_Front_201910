27ba2701c9c6629681b27f8651fc4d11cfe31390
JuanFelipePena <jf.pena10@uniandes.edu.co>
2019-05-21 20:53:08 -0500

    Update blog-deail.ts

--- src/app/blog/blog-deail.ts
+++ src/app/blog/blog-deail.ts
@@ -1,9 +1,11 @@
+import { Blog } from './../blog/blog';
 
-export class BlogDetail {
-  
+
+export class BlogDetail extends Blog{
+
     id: number;
     fecha  : Date;
     texto: string;
-    
+
 
 }
